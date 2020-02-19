393a2aaedd7f6350de868967bd0e2e42421c8423
Alan Ruiz <gheryk90@gmail.com>
2020-02-19 17:10:58 +0100

    need styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+restaurant_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  93833737,
+    category:      "english"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  93837272,
+    category:       "pizza"
+  }
+]
+
+resto = Restaurant.create!(restaurant_attributes)
+
+puts "Finished creating #{resto.count} restaurants."
